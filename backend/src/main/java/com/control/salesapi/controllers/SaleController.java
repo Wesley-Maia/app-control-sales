3926a5e3ec07acb369e6701af939731934a0ff3b
Wesley Maia <maia.weol@gmail.com>
2022-07-20 15:18:56 -0700

    Date Select

--- backend/src/main/java/com/control/salesapi/controllers/SaleController.java
+++ backend/src/main/java/com/control/salesapi/controllers/SaleController.java
@@ -3,8 +3,11 @@ package com.control.salesapi.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.control.salesapi.entities.Sale;
@@ -16,8 +19,16 @@ public class SaleController {
 
 	@Autowired
 	private SaleService saleService;
-
+	
 	@GetMapping
+	public Page<Sale> findSales(
+			@RequestParam(value="minDate", defaultValue = "") String minDate,
+			@RequestParam(value="maxDate", defaultValue = "") String maxDate,
+			Pageable pageable) {
+		return saleService.findSales(minDate, maxDate, pageable);
+	}
+
+	@GetMapping(value="/all")
 	public List<Sale> findAll() {
 		return saleService.findAll();
 	}
